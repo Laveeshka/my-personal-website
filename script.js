0a6e3adc0229b7e82777e9f3ed43f9345082b718
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-26 12:55:30 +1000

    added spicy hamburger menu

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.getElementById('nav').addEventListener('click', function (e) {
         document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
     }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+const nav = document.querySelector('#nav');
+const navWrap = document.querySelector('div.nav-wrap');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navigation.classList.toggle("active");
+    nav.classList.toggle("active");
+    navWrap.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".link").forEach(link => link.
+    addEventListener("click", () => {
+        console.log("click");
+        hamburger.classList.toggle("active");
+        navigation.classList.toggle("active");
+
+        nav.classList.toggle("active");
+        navWrap.classList.toggle("active");
+    
+    }))
